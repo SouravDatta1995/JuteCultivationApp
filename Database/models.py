36536e48e684045e0d33818d56058af902b35428
Soumik <soumiksanyal95@gmail.com>
2020-09-23 16:09:44 +0530

    table created

--- Database/models.py
+++ Database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, NUMERIC, Boolean, Numeric, DateTime
 
 from .database import Base
 
@@ -16,3 +16,33 @@ class Subject(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, nullable=False)
+
+
+class JuteSurveyInputs(Base):
+    __tablename__ = "jutesurveyinputs"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    plot = Column(String, nullable=False)
+    plotarea = Column(Integer, nullable=False)
+    soiltype = Column(String)
+    soilhealthcard = Column(Boolean)
+    nitrogenamount = Column(Numeric)
+    phosphorousamount = Column(Numeric)
+    pottasiumamount = Column(Numeric)
+    organiccarbonamount = Column(Numeric)
+    dateofsowing = Column(DateTime)
+    methodofsowing = Column(Integer)
+    varietyused = Column(Integer)
+    methodofweeding = Column(Integer)
+    amountofseed = Column(Integer)
+    dateofharvest = Column(DateTime)
+    daysleftforharvest = Column(Integer)
+    methodofretting = Column(Integer)
+    sourceofwater = Column(Integer)
+    availabilityofwater = Column(Boolean)
+    qualityofwashingwater = Column(Boolean)
+    daysrequiredforcompleteretting = Column(String)
+    rainfall = Column(Numeric)
+    temperature = Column(String)
+    humidity = Column(String)
